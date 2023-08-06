7bd71dcda65a23fd33f8306ac89c832fac6dc3d6
thriwax <91975849+thriwax@users.noreply.github.com>
2023-08-06 06:25:02 +0300

    13

--- components/Links/Links.tsx
+++ components/Links/Links.tsx
@@ -6,9 +6,9 @@ import Image from 'next/image'
 const Links = () => {
   return (
     <div className='links-wrapper'>
-        <a href='https://soundcloud.com/khoroslabel' target='_blank'><Image src={scIcon} width={120} height={120} alt='test' /></a>
-        <a href='https://www.instagram.com/xor.su/' target='_blank'><Image src={instIcon} width={120} height={120} alt='test' /></a>
-        <a href='https://khoros.bandcamp.com/music' target='_blank'><Image src={bcIcon} width={120} height={120} alt='test' /></a>
+        <a href='https://soundcloud.com/khoroslabel' target='_blank'><Image src={scIcon} width={80} height={80} alt='test' /></a>
+        <a href='https://www.instagram.com/xor.su/' target='_blank'><Image src={instIcon} width={80} height={80} alt='test' /></a>
+        <a href='https://khoros.bandcamp.com/music' target='_blank'><Image src={bcIcon} width={80} height={80} alt='test' /></a>
     </div>
   )
 }
